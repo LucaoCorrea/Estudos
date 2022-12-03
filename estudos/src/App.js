1d3186009c151991b23212e8a9834cfa90259733
LucaoCorrea <lucascorrea1505@gmail.com>
2022-12-03 14:56:46 -0300

    commit aula 09

--- estudos/src/App.js
+++ estudos/src/App.js
@@ -1,28 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import HelloWorld from "./components/HelloWorld";
-import SayMyName from "./components/SayMyName";
-import Pessoa from "./components/Pessoa";
-import Frase from "./components/Frase";
+import Evento from "./components/Evento";
+import Form from "./components/Form";
 
 function App() {
-  const nome = "Maria";
-
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase/>
-      <Frase/>
-      <HelloWorld />
-      <SayMyName nome="Lucas" />
-      <SayMyName nome="Matheus" />
-      <SayMyName nome={nome} />
-      <Pessoa
-        nome="Rodrigo"
-        idade="28"
-        profissao="Programador"
-        foto="https://via.placeholder.com/150"
-      />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
     </div>
   );
 }
